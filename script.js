9791455e315ac842df1c54ee63c9aa396fce50d2
SubhradeepSeth <139810175+SubhradeepSeth@users.noreply.github.com>
2023-11-06 11:27:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+document.getElementById("openModal").addEventListener("click", function () {
+    document.getElementById("modal").style.display = "block";
+});
+
+document.getElementById("closeModal").addEventListener("click", function () {
+    document.getElementById("modal").style.display = "none";
+});
+
+document.addEventListener("click", function (e) {
+    if (e.target === document.getElementById("modal")) {
+        document.getElementById("modal").style.display = "none";
+    }
+});
